30a1ad91e2fb092effaf0f97ad3bfff7c431696c
Joan Bosch Adell <joan.bosch@softtek.com>
2021-01-30 14:11:24 +0100

    pow iterative and recursion function

---
+++ Part1/pow.js
@@ -0,0 +1,23 @@
+const powIterative = function(n,e) {
+    let result = 1;
+    let isNegative = e < 0;
+    e = isNegative ? -e : e;
+    for(let i = 0; i < e; i += 1) {
+        result *= n;
+    }
+    return isNegative ? 1 / result : result;
+};
+
+const pow = function(n,e) {
+    const powAux = (n, e, acum) => e < 1 ? 
+                                            acum:
+                                            powAux(n, e - 1, n * acum);
+    return e >= 0 ? powAux(n,e,1) : 1 / powAux(n, -e, 1);
+} 
+
+console.log(
+    powIterative(2,3),
+    pow(2,3),
+    powIterative(2,-2),
+    pow(2,-2)
+)
